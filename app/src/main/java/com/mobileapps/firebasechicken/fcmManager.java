80d54081f7601555878566dc4d0174bba225aa97
alexander ebigwei <gentlealex48@yahoo.com>
2019-03-17 15:35:17 -0400

    Initial commit

---
+++ app/src/main/java/com/mobileapps/firebasechicken/fcmManager.java
@@ -0,0 +1,22 @@
+package com.mobileapps.firebasechicken;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static com.mobileapps.firebasechicken.PeopleActivity.TAG;
+
+public class fcmManager extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+    }
+}
